d99d71660ba94a00ff83c392121f543fdeb693f0
Annestezia <cast.wheel@gmail.com>
2019-11-17 15:37:37 +1000

    remasterred   version,  slow(why) , mostly ready

--- src/App.js
+++ src/App.js
@@ -18,34 +18,31 @@ import landing_page_v2_bg from './images/landing-page-v2-preview.png';
 
 const App = (props) => {
   const projects = [
-    {name:"Twitch TV", description:"JQuery, Twitch API",link:"https://annestezia.github.io/TwitchTV/",level:"span-2",bg: twitch_tv_bg},
-    {name:"Markdown Previewer", description:"marked.js",link:"https://annestezia.github.io/fcc-markdown-previewer/", level:"span-2", bg: markdown_previewer_bg},
-    {name:"Drum Machine", description:"React",link:"https://annestezia.github.io/fcc-drum-machine/",level:"span-2", bg: drum_machine_bg},
-    {name:"Quote Machine", description:"fetch, Quotesondesign API",link:"https://annestezia.github.io/fcc-quote-machine/",level:"span-1",bg: quote_machine_bg},
-    {name:"Local Weather App", description:"Dark Sky weather API, ip geolocation, jQuery",link:"https://annestezia.github.io/Local-weather-app/",level:"span-2", bg: local_weather_app_bg},
-    {name:"Tech Docs", description:"Tech Docs, adaptive",link:"https://codepen.io/Annestezia/full/aKZmQO",level:"span-1", bg: tech_docs_bg},
-    {name:"Wiki Viewer", description:"Wikipedia API, jQuery",link:"https://annestezia.github.io/WikiViewer/",level:"span-1", bg: wiki_viewer_bg},
-    {name:"Landing Page v2", description:"Waypoints.js, Feather icons",link:"https://annestezia.github.io/fcc-lp2/",level:"span-1", bg: landing_page_v2_bg},
-    {name:"Tribute Page ", description:"HTML, Bootstrap, Font Awesome",link:"https://annestezia.github.io/TributePage/",level:"span-1", bg: tribute_page_bg},
-    {name:"Survey Form", description:"Form", link:"http://github.com", level:"span-1", bg: survey_form_bg  },
-    {name:"Landing Page", description:"Waypoints.js, Feather icons",link:"https://annestezia.github.io/fcc-lp2/",level:"span-1", bg: landing_page_bg}
+    { title:"Twitch TV", description:"JQuery, Twitch API",link:"https://annestezia.github.io/TwitchTV/",level:"span-2",bg: twitch_tv_bg},
+    { title:"Markdown Previewer", description:"marked.js",link:"https://annestezia.github.io/fcc-markdown-previewer/", level:"span-2", bg: markdown_previewer_bg},
+    { title:"Drum Machine", description:"React, SCSS",link:"https://annestezia.github.io/fcc-drum-machine/",level:"span-2", bg: drum_machine_bg},
+    { title:"Quote Machine", description:"Quotesondesign API, Twitter API",link:"https://annestezia.github.io/fcc-quote-machine/",level:"span-1",bg: quote_machine_bg},
+    { title:"Local Weather App", description:"Dark Sky weather API, ip geolocation, jQuery",link:"https://annestezia.github.io/Local-weather-app/",level:"span-2", bg: local_weather_app_bg},
+    { title:"Tech Docs", description:"Tech Docs, SCSS, prism.js, adaptive",link:"https://codepen.io/Annestezia/full/aKZmQO",level:"span-1", bg: tech_docs_bg},
+    { title:"Wiki Viewer", description:"Wikipedia API, jQuery",link:"https://annestezia.github.io/WikiViewer/",level:"span-1", bg: wiki_viewer_bg},
+    { title:"Landing Page v2", description:"Waypoints.js, Feather icons",link:"https://annestezia.github.io/fcc-lp2/",level:"span-1", bg: landing_page_v2_bg},
+    { title:"Tribute Page ", description:"HTML, Bootstrap, Font Awesome",link:"https://annestezia.github.io/TributePage/",level:"span-1", bg: tribute_page_bg},
+    { title:"Survey Form", description:"HTML5, animate.css", link:"https://codepen.io/Annestezia/full/JZKKVV", level:"span-1", bg: survey_form_bg  },
+    { title:"Landing Page", description:"Waypoints.js, Feather icons",link:"https://annestezia.github.io/fcc-lp2/",level:"span-1", bg: landing_page_bg}
   ];
 
   return(
 
   <>
     {projects.map(project => (
-      <div className= {`project-tile ${project.level}`} key={project.name} id = {
-          project.name.split(' ').join('-').toLowerCase()} style={{
-          backgroundImage:`url(${project.bg})`,
-          backgroundPosition: 'center',
-          backgroundSize: 'cover',
-          backgroundRepeat: 'no-repeat'
+      <div className= {`project-tile ${project.level}`} key={project.title} id = {
+          project.title.split(' ').join('-').toLowerCase()} style={{
+          backgroundImage:`url(${project.bg})`
         }}>
-        <a href={project.link} title= "Open repo or  gh-page">
+        <a href={project.link} title= {`${project.title} demo`} target="_blank">
           <div className="overlay">
-            <h3 className="project-title">{project.name}</h3>
-            <span className = "description">{project.description}</span>
+            <h3 className="project-title">{project.title}</h3>
+            <span className = "project-description">{project.description}</span>
           </div>
         </a>
       </div>
