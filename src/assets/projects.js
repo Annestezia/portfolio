0abc3498dd9c6975dee625e921e14f49e230f903
Annestezia <cast.wheel@gmail.com>
2020-07-02 15:14:53 +1000

    added assets folder with images, projects list, dictonary(todo)

---
+++ src/assets/projects.js
@@ -0,0 +1,100 @@
+import bar_chart_bg from './images/d3-barchart-preview.png';
+import drum_machine_bg from './images/drum-machine-preview.png';
+import twitch_tv_bg from './images/twitch-tv-preview.jpg';
+import markdown_previewer_bg from './images/markdown-previewer-preview.png';
+import quote_machine_bg from './images/quote-machine-preview.png';
+import local_weather_app_bg from './images/local-weather-app-preview.jpg';
+import survey_form_bg from './images/survey-form-preview.png';
+import wiki_viewer_bg from './images/wiki-viewer-preview.png';
+import landing_page_bg from './images/landing-page-preview.png';
+import tech_docs_bg from './images/tech-docs-preview.png';
+import tribute_page_bg from './images/tribute-page-preview.png';
+import landing_page_v2_bg from './images/landing-page-v2-preview.png';
+
+
+export default [
+	{ 
+		title:"D3 barchart",
+		description:"d3.js",
+		link:"https://annestezia.github.io/fcc-d3-barchart/",
+		level:"span-1",
+		bg: bar_chart_bg
+	},
+	{ 
+		title:"Twitch TV",
+		description:"JQuery, Twitch API",
+		link:"https://annestezia.github.io/TwitchTV/",
+		level:"span-2",
+		bg: twitch_tv_bg
+	},
+	{
+		title:"Quote Machine",
+		description:"Twitter API",
+		link:"https://annestezia.github.io/fcc-quote-machine/",
+		level:"span-1",
+		bg: quote_machine_bg
+	},
+	{
+		title:"Local Weather App",
+		description:"Dark Sky weather API, geolocation, jQuery",
+		link:"https://annestezia.github.io/Local-weather-app/",
+		level:"span-2",
+		bg: local_weather_app_bg
+	},
+	{
+		title:"Tech Docs",
+		description:"Tech Docs, SCSS, prism.js, adaptive",
+		link:"https://codepen.io/Annestezia/full/aKZmQO",
+		level:"span-2",
+		bg: tech_docs_bg
+	},
+	{ 
+		title:"Markdown Previewer",
+		description:"marked.js",
+		link:"https://annestezia.github.io/fcc-markdown-previewer/",
+		level:"span-1",
+		bg: markdown_previewer_bg
+	},
+	{
+		title:"Wiki Viewer",
+		description:"Wikipedia API, jQuery",
+		link:"https://annestezia.github.io/WikiViewer/",
+		level:"span-1",
+		bg: wiki_viewer_bg
+	},
+	{ 
+		title:"Drum Machine",
+		description:"React, SCSS",
+		link:"https://annestezia.github.io/fcc-drum-machine/",
+		level:"span-2",
+		bg: drum_machine_bg
+	},
+	{
+		title:"Landing Page EcoSquad",
+		description:"Feather icons, animate.css",
+		link:"https://annestezia.github.io/fcc-lp2/",
+		level:"span-1",
+		bg: landing_page_v2_bg
+	},
+	{
+		title:"Tribute Page ",
+		description:"Bootstrap, Font Awesome",
+		link:"https://annestezia.github.io/TributePage/",
+		level:"span-1",
+		bg: tribute_page_bg
+	},
+	{
+		title:"Survey Form",
+		description:"HTML5, animate.css",
+		link:"https://codepen.io/Annestezia/full/JZKKVV",
+		level:"span-1",
+		bg: survey_form_bg
+	},
+	{
+		title:"Landing Page Smartwatch",
+		description:"Waypoints.js",
+		link:"https://github.com/Annestezia/fcc-lp",
+		level:"span-1",
+		bg: landing_page_bg
+	}
+];
