c020de4ca3c8b9cb795c884e84746dbb7748aaa8
Annestezia <cast.wheel@gmail.com>
2020-11-23 15:23:21 +1000

    fix render, rename App=>ProjectsList

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import ProjectsList from './components/ProjectsList';
+// import SkillsList from './components/SkillsList';
 import * as serviceWorker from './serviceWorker';
+// import BadgesList from './Project/BadgesList';
+// import skills from '../src/assets/skills';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ProjectsList />, document.getElementById('root'));
+// ReactDOM.render(<SkillsList />, document.getElementById('about__skills-list'));
+// ReactDOM.render(<BadgesList badges={skills}/>, document.getElementById('skills'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
