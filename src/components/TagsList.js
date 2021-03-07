e486a4a32b1d8b5823e4d19a6ac76b7818607c96
Annestezia <cast.wheel@gmail.com>
2021-03-07 19:06:14 +1000

    moved styling to component stylesheet

--- src/components/TagsList.js
+++ src/components/TagsList.js
@@ -3,7 +3,7 @@ import './TagsList.scss';
 
 export default function TagsList({tags, parent}){  
   const listItems = tags.map((tag,i) =>
-    <span key={i} className={`${parent} tag p-sm m-xs`}>
+    <span key={i} className={`${parent} tag`}>
       {tag}    
     </span>
   );
