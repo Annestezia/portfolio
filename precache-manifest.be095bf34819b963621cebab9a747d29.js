fd026a5c268804663eed7919c7a5f8026accf384
Annestezia <cast.wheel@gmail.com>
2021-03-10 18:25:55 +1000

    Updates

---
+++ precache-manifest.be095bf34819b963621cebab9a747d29.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c9f728f0a308e19207d",
+    "url": "/portfolio/static/css/main.34f8bc99.chunk.css"
+  },
+  {
+    "revision": "6c9f728f0a308e19207d",
+    "url": "/portfolio/static/js/main.b9f2fa5d.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "8ef0af75308975707188",
+    "url": "/portfolio/static/js/2.52ddf079.chunk.js"
+  },
+  {
+    "revision": "7de565ec72dbb893a40107b2132dad9c",
+    "url": "/portfolio/static/media/d3-barchart-preview.7de565ec.png"
+  },
+  {
+    "revision": "739454868bed9cb91741434a7c2c0eda",
+    "url": "/portfolio/static/media/drum-machine-preview.73945486.png"
+  },
+  {
+    "revision": "7571b0d4896c0f0d83cc58ac0eb14862",
+    "url": "/portfolio/static/media/twitch-tv-preview.7571b0d4.jpg"
+  },
+  {
+    "revision": "b66e64d8261ecce15f4c9a83b2dcea0a",
+    "url": "/portfolio/static/media/markdown-previewer-preview.b66e64d8.png"
+  },
+  {
+    "revision": "fd9b2068fa800deae82988438d6f1f47",
+    "url": "/portfolio/static/media/quote-machine-preview.fd9b2068.png"
+  },
+  {
+    "revision": "e9594c7c66a41ae632dcf7a04857e878",
+    "url": "/portfolio/static/media/weather-app-preview.e9594c7c.png"
+  },
+  {
+    "revision": "7f8886e9a4555073de38de565b52da31",
+    "url": "/portfolio/static/media/survey-form-preview.7f8886e9.png"
+  },
+  {
+    "revision": "02d24f7b35d419ea793d03dc88a70829",
+    "url": "/portfolio/static/media/wiki-viewer-preview.02d24f7b.png"
+  },
+  {
+    "revision": "628cdefcf55418c46ddcd92a73468e6e",
+    "url": "/portfolio/static/media/landing-page-preview.628cdefc.png"
+  },
+  {
+    "revision": "031a1b685fc6875f858ba13431c8fc5b",
+    "url": "/portfolio/static/media/tech-docs-preview.031a1b68.png"
+  },
+  {
+    "revision": "4ff8fc37dbe0c624e234e92ce32ad13d",
+    "url": "/portfolio/static/media/tribute-page-preview.4ff8fc37.png"
+  },
+  {
+    "revision": "bc4c418039f2361b5df45a1a11261610",
+    "url": "/portfolio/static/media/landing-page-v2-preview.bc4c4180.png"
+  },
+  {
+    "revision": "b987bc3b71773ca3e2d6a6c3358bc99b",
+    "url": "/portfolio/static/media/js-calc-preview.b987bc3b.png"
+  },
+  {
+    "revision": "57dbd1fcb49e769bc512afad11b0e8ce",
+    "url": "/portfolio/index.html"
+  }
+];
